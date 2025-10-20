f5b58dea401a972e24df1adc0e3894e6493865c5
Gideon Bempong <gideonbempong533@gmail.com>
2025-10-20 22:10:53 +0000

    members ui (#11)

--- apps/property-manager/app/modules/properties/property/settings/members/index.tsx
+++ apps/property-manager/app/modules/properties/property/settings/members/index.tsx
@@ -1,3 +1,211 @@
+import type { ColumnDef } from '@tanstack/react-table'
+import { CircleCheck, CircleX, EllipsisVertical, User } from 'lucide-react'
+import { useMemo } from 'react'
+import { MembersController } from './controller'
+import { DataTable } from '~/components/datatable'
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '~/components/ui/alert-dialog'
+import { Badge } from '~/components/ui/badge'
+import { Button } from '~/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '~/components/ui/dropdown-menu'
+import { TypographyH4, TypographyMuted } from '~/components/ui/typography'
+
 export function PropertyMembersModule() {
-	return <div>Property Members</div>
+	const columns: ColumnDef<ClientUser>[] = useMemo(() => {
+		return [
+			{
+				id: 'drag',
+				header: () => null,
+				cell: () => <User />,
+			},
+			{
+				accessorKey: 'name',
+				header: 'Name',
+				cell: ({ getValue }) => {
+					return (
+						<div className="min-w-32">
+							<span className="truncate text-xs text-zinc-600">
+								{getValue<string>()}
+							</span>
+						</div>
+					)
+				},
+				enableHiding: false,
+			},
+			{
+				accessorKey: 'role',
+				header: 'Role',
+				cell: ({ getValue }) => (
+					<Badge variant="outline" className="text-muted-foreground px-1.5">
+						<span className="truncate text-xs text-zinc-600">
+							{getValue<string>()}
+						</span>
+					</Badge>
+				),
+			},
+			{
+				accessorKey: 'id',
+				header: 'Contact',
+				cell: ({ row }) => (
+					<div className="flex min-w-32 flex-col items-start gap-1">
+						<span className="truncate text-xs text-zinc-600">
+							{row.original.email}
+						</span>
+						<span className="truncate text-xs text-zinc-600">
+							{row.original.phone_number}
+						</span>
+					</div>
+				),
+			},
+
+			{
+				accessorKey: 'status',
+				header: 'Status',
+				cell: ({ getValue }) => (
+					<Badge variant="outline" className="text-muted-foreground px-1.5">
+						{getValue<string>() === 'ClientUser.Status.Active' ? (
+							<CircleCheck className="fill-green-600 text-white" />
+						) : (
+							<CircleX className="fill-red-500 text-white" />
+						)}
+						{getValue<string>() === 'ClientUser.Status.Active'
+							? 'Active'
+							: 'Inactive'}
+					</Badge>
+				),
+			},
+			{
+				id: 'actions',
+				cell: () => (
+					<AlertDialog>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button
+									variant="ghost"
+									className="data-[state=open]:bg-muted text-muted-foreground flex size-8"
+									size="icon"
+								>
+									<EllipsisVertical />
+									<span className="sr-only">Open menu</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end" className="w-32">
+								<DropdownMenuItem>Edit</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<AlertDialogTrigger asChild>
+									<DropdownMenuItem variant="destructive">
+										Deactivate
+									</DropdownMenuItem>
+								</AlertDialogTrigger>
+							</DropdownMenuContent>
+						</DropdownMenu>
+						<AlertDialogContent className="sm:max-w-[425px]">
+							<AlertDialogHeader>
+								<AlertDialogTitle>Are you sure?</AlertDialogTitle>
+								<AlertDialogDescription>
+									This will deactivate the member.
+								</AlertDialogDescription>
+							</AlertDialogHeader>
+							<AlertDialogFooter className="mt-5">
+								<AlertDialogCancel>Cancel</AlertDialogCancel>
+								<AlertDialogAction className="bg-destructive hover:bg-destructive/90 text-white">
+									Deactivate
+								</AlertDialogAction>
+							</AlertDialogFooter>
+						</AlertDialogContent>
+					</AlertDialog>
+				),
+			},
+		]
+	}, [])
+
+	return (
+		<main className="flex flex-col gap-2 sm:gap-4">
+			<div>
+				<TypographyH4>Manage Members</TypographyH4>
+				<TypographyMuted>
+					These members have access to your workspace.
+				</TypographyMuted>
+			</div>
+			<MembersController />
+			<div className="h-full w-full">
+				<DataTable
+					columns={columns}
+					dataResponse={{
+						rows: [
+							{
+								id: '1',
+								name: 'John Doe',
+								email: 'john.doe@example.com',
+								phone_number: '123-456-7890',
+								role: 'OWNER',
+								created_at: new Date(),
+								updated_at: new Date(),
+								client: null,
+								client_id: '1',
+								status: 'ClientUser.Status.Active',
+							},
+							{
+								id: '1',
+								name: 'John Doe',
+								email: 'john.doe@example.com',
+								phone_number: '123-456-7890',
+								role: 'OWNER',
+								created_at: new Date(),
+								updated_at: new Date(),
+								client: null,
+								client_id: '1',
+								status: 'ClientUser.Status.Active',
+							},
+							{
+								id: '1',
+								name: 'John Doe',
+								email: 'john.doe@example.com',
+								phone_number: '123-456-7890',
+								role: 'OWNER',
+								created_at: new Date(),
+								updated_at: new Date(),
+								client: null,
+								client_id: '1',
+								status: 'ClientUser.Status.Active',
+							},
+						],
+						total: 150,
+						page: 1,
+						page_size: 50,
+						order: 'desc',
+						order_by: 'created_at',
+						has_prev_page: false,
+						has_next_page: true,
+					}}
+					empty={{
+						message: 'No members found',
+						description:
+							"Try adjusting your search or filter to find what you're looking for.",
+						button: {
+							label: 'Add Member',
+							onClick: () => {
+								// Handle button click
+							},
+						},
+					}}
+				/>
+			</div>
+		</main>
+	)
 }
