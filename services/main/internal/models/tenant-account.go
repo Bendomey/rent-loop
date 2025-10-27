a20b9ef3b9d62625ec6164c4c2523fcbf4c62eda
Domey Benjamin <domeybenjamin1@gmail.com>
2025-10-27 09:58:02 +0000

    chore: models init

--- services/main/internal/models/tenant-account.go
+++ services/main/internal/models/tenant-account.go
@@ -3,7 +3,8 @@ package models
 // TenantAccount represents a tenant user in the system
 type TenantAccount struct {
 	BaseModelSoftDelete
-	Name  string `json:"name" gorm:"not null;"`
-	Phone string `json:"phone" gorm:"not null;uniqueIndex"`
-	Email string `json:"email" gorm:"index"`
+	TenantId string `json:"tenantId" gorm:"not null;uniqueIndex;"`
+	Tenant   Tenant
+
+	PhoneNumber string `json:"phoneNumber" gorm:"not null;uniqueIndex;"`
 }
