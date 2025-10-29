8298934c3007b77764df10b0603dd967746d6a72
Domey Benjamin <domeybenjamin1@gmail.com>
2025-10-29 15:43:14 +0000

    chore: added fetching data in middleware

--- apps/property-manager/app/lib/actions/auth.middleware.server.ts
+++ apps/property-manager/app/lib/actions/auth.middleware.server.ts
@@ -1,6 +1,7 @@
 import { redirect, type MiddlewareFunction } from 'react-router'
 import { userContext } from './auth.context.server'
 import { deleteAuthSession, getAuthSession } from './auth.session.server'
+import { USER_CACHE_KEY, userCache } from './cache.server'
 import { environmentVariables } from './env.server'
 import { getCurrentUser } from '~/api/auth'
 
@@ -20,7 +21,16 @@ export const authMiddleware: MiddlewareFunction = async ({
 		return redirect(redirectToLogin)
 	}
 
+	const cacheKey = USER_CACHE_KEY.replace('{token}', authToken)
+
 	try {
+		const cached = userCache.get(cacheKey)
+		if (cached) {
+			const clientUserParsed = JSON.parse(cached as string)
+			context.set(userContext, clientUserParsed)
+			return
+		}
+
 		const clientUser = await getCurrentUser({
 			baseUrl,
 			authToken,
@@ -30,9 +40,11 @@ export const authMiddleware: MiddlewareFunction = async ({
 			throw new Error('No user found')
 		}
 
+		userCache.set(cacheKey, JSON.stringify(clientUser))
 		context.set(userContext, clientUser)
 	} catch {
 		// if there're any errors fetching the user, logout please!
+		userCache.delete(cacheKey)
 		return redirect(redirectToLogin, {
 			headers: {
 				'Set-Cookie': await deleteAuthSession(authSession),
