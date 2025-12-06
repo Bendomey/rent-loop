7d6d98f2846d2c1c2b800006326d0e3f8c382bd1
EbenDomey <domeyeben4@gmail.com>
2025-12-06 20:22:58 +0000

    implemented ListUnits endpoint

--- services/main/internal/services/unit.go
+++ services/main/internal/services/unit.go
@@ -3,11 +3,13 @@ package services
 import (
 	"context"
 
+	"github.com/Bendomey/rent-loop/services/main/internal/models"
 	"github.com/Bendomey/rent-loop/services/main/internal/repository"
 	"github.com/Bendomey/rent-loop/services/main/pkg"
 )
 
 type UnitService interface {
+	ListUnits(context context.Context, filterQuery repository.ListUnitsFilter) ([]models.Unit, error)
 	CountUnits(context context.Context, filterQuery repository.ListUnitsFilter) (int64, error)
 }
 
@@ -18,6 +20,21 @@ type unitService struct {
 
 func NewUnitService(appCtx pkg.AppContext, repo repository.UnitRepository) UnitService {
 	return &unitService{appCtx: appCtx, repo: repo}
+}
+
+func (s *unitService) ListUnits(ctx context.Context, filterQuery repository.ListUnitsFilter) ([]models.Unit, error) {
+	units, listUnitsErr := s.repo.List(ctx, filterQuery)
+	if listUnitsErr != nil {
+		return nil, pkg.InternalServerError(listUnitsErr.Error(), &pkg.RentLoopErrorParams{
+			Err: listUnitsErr,
+			Metadata: map[string]string{
+				"function": "ListUnits",
+				"action":   "listing units",
+			},
+		})
+	}
+
+	return *units, nil
 }
 
 func (s *unitService) CountUnits(
