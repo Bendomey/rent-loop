0e0c6d64753bd2810a2fd14f7e7a3ce46796346b
gidoo5331 <gideonbempong533@gmail.com>
2025-12-18 13:49:55 +0000

    delete unit

--- apps/property-manager/app/api/units/index.ts
+++ apps/property-manager/app/api/units/index.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { QUERY_KEYS } from '~/lib/constants'
 import { getQueryParams } from '~/lib/get-param'
 import { fetchClient } from '~/lib/transport'
@@ -41,4 +41,35 @@ export const useGetPropertyUnits = (
 	useQuery({
 		queryKey: [QUERY_KEYS.PROPERTY_UNITS, query],
 		queryFn: () => getPropertyUnits(query),
+	})
+
+/**
+ * Delete property unit
+ */
+const deletePropertyUnit = async (props: {
+	property_id: string
+	unit_id: string
+}) => {
+	try {
+		await fetchClient(
+			`/v1/properties/${props.property_id}/units/${props.unit_id}`,
+			{
+				method: 'DELETE',
+			},
+		)
+	} catch (error: unknown) {
+		if (error instanceof Response) {
+			const response = await error.json()
+			throw new Error(response.errors?.message || 'Unknown error')
+		}
+
+		if (error instanceof Error) {
+			throw error
+		}
+	}
+}
+
+export const useDeletePropertyUnit = () =>
+	useMutation({
+		mutationFn: deletePropertyUnit,
 	})
