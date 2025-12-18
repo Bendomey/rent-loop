2dd585c822a9b28c0ad2e6c4e47a6225aa8c8b5c
gidoo5331 <gideonbempong533@gmail.com>
2025-12-18 13:12:54 +0000

    add block filter

--- apps/property-manager/app/routes/_auth.properties.$propertyId.assets.units._index.ts
+++ apps/property-manager/app/routes/_auth.properties.$propertyId.assets.units._index.ts
@@ -1,15 +1,32 @@
 import type { Route } from './+types/_auth.properties.$propertyId.assets.units._index'
+import { getPropertyBlocksForServer } from '~/api/blocks/server'
+import { getAuthSession } from '~/lib/actions/auth.session.server'
+import { environmentVariables } from '~/lib/actions/env.server'
 import { propertyContext } from '~/lib/actions/property.context.server'
 import { getDisplayUrl, getDomainUrl } from '~/lib/misc'
 import { getSocialMetas } from '~/lib/seo'
+import { safeString } from '~/lib/strings'
 import { PropertyAssetUnitsModule } from '~/modules'
 
 export async function loader({ request, context }: Route.LoaderArgs) {
 	const clientUserProperty = context.get(propertyContext)
+	const baseUrl = environmentVariables().API_ADDRESS
+	const authSession = await getAuthSession(request.headers.get('Cookie'))
+	const authToken = authSession.get('authToken')
+	const property_id = safeString(clientUserProperty?.property?.id)
+
+	const blocks = await getPropertyBlocksForServer(
+		{ property_id },
+		{
+			authToken,
+			baseUrl,
+		},
+	)
 
 	return {
 		origin: getDomainUrl(request),
 		clientUserProperty,
+		blocks,
 	}
 }
 
