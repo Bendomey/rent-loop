b43e3c2a83c6d488a2cc87548d510c9fbe094587
Domey Benjamin <domeybenjamin1@gmail.com>
2025-12-23 11:40:59 +0000

    feat: update pubspec.yaml with new dependencies and improve project description
    chore: register new plugins in generated_plugin_registrant.cc for Windows
    
    build: update generated_plugins.cmake to include new plugins
    
    build: add Makefile for build automation tasks
    
    chore: create Podfile for iOS with necessary configurations
    
    chore: create Podfile for macOS with necessary configurations

--- apps/go/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/go/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import flutter_secure_storage_macos
+import package_info_plus
+import path_provider_foundation
+import sentry_flutter
+import share_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
