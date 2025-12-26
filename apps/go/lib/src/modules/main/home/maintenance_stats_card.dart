08785f609bf2989e46d5ab217324839a73e45ac5
Domey Benjamin <domeybenjamin1@gmail.com>
2025-12-26 07:46:26 +0000

    feat: add MaintenanceStatsCard and UplomingPaymentCard components

---
+++ apps/go/lib/src/modules/main/home/maintenance_stats_card.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class StatItem {
+  final String title;
+  final String count;
+  final IconData icon;
+  final Color bgColor;
+  final Color color;
+
+  StatItem({
+    required this.title,
+    required this.count,
+    required this.icon,
+    required this.bgColor,
+    required this.color,
+  });
+}
+
+class MaintenanceStatsCard extends StatelessWidget {
+  const MaintenanceStatsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<StatItem> stats = [
+      StatItem(
+        title: 'Open Issues',
+        count: '2',
+        icon: Icons.report_problem,
+        bgColor: Color.fromARGB(180, 239, 108, 0),
+        color: Color.fromARGB(180, 239, 108, 0),
+      ),
+      StatItem(
+        title: 'Resolved Issues',
+        count: '5',
+        icon: Icons.build,
+        bgColor: Color.fromARGB(255, 3, 117, 119),
+        color: Color.fromARGB(255, 5, 133, 136),
+      ),
+      StatItem(
+        title: 'Pending Approvals',
+        count: '1',
+        icon: Icons.pending_actions,
+        bgColor: Colors.indigo,
+        color: Colors.indigo.shade400,
+      ),
+    ];
+
+    return Card(
+      color: Colors.white,
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: BorderSide(color: Colors.grey.shade100, width: 1),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(00),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Maintenance Stats',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                fontSize: 17,
+                fontFamily: "Shantell",
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Here is a summary of your maintenance requests and their statuses.',
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
+            SizedBox(height: 15),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 7,
+                childAspectRatio: 0.65,
+              ),
+              itemCount: stats.length,
+              itemBuilder: (context, index) {
+                final stat = stats[index];
+
+                return Container(
+                  height: 300,
+                  // border raduis
+                  decoration: BoxDecoration(
+                    color: stat.bgColor,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 13,
+                            vertical: 13,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(150),
+                            color: stat.color,
+                          ),
+                          child: Icon(stat.icon, color: Colors.white, size: 15),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              stat.count,
+                              style: Theme.of(context).textTheme.displayMedium!
+                                  .copyWith(color: Colors.white, fontSize: 30),
+                            ),
+                            Text(
+                              stat.title,
+                              style: Theme.of(context).textTheme.labelLarge!
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- apps/go/lib/src/modules/main/home/root.dart
+++ apps/go/lib/src/modules/main/home/root.dart
@@ -1,5 +1,7 @@
 import 'package:rentloop_go/src/architecture/architecture.dart';
 import 'package:flutter/material.dart';
+import 'maintenance_stats_card.dart';
+import 'upcoming_payment_card.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -13,12 +15,74 @@ class _HomeScreen extends ConsumerState<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Overview',
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 20),
-        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+              icon: Badge.count(
+                count: 4,
+                backgroundColor: Colors.red,
+                child: Icon(Icons.notifications_outlined),
+              ),
+              onPressed: () async {
+                await Haptics.vibrate(HapticsType.selection);
+              },
+            ),
+          ),
+        ],
       ),
-      body: Text('Home Screen'),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              'Welcome back, Benjamin!',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: MaterialBanner(
+              content: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Announcement",
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "Your rent for June is due in 5 days. Please ensure timely payment to avoid late fees.",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+              leading: const Icon(Icons.info),
+              padding: EdgeInsets.all(10),
+              backgroundColor: Colors.grey.shade100,
+              actions: [
+                IconButton(
+                  onPressed: () =>
+                      ScaffoldMessenger.of(context).hideCurrentMaterialBanner(),
+                  icon: const Icon(Icons.remove_red_eye, color: Colors.red),
+                ),
+              ],
+              dividerColor:
+                  Colors.transparent, // removes bottom divider entirely
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: UplomingPaymentCard(),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: MaintenanceStatsCard(),
+          ),
+        ],
+      ),
     );
   }
 }
