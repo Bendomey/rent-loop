aa46b33b3e01c5d125582a7a3162afe22dfbef39
Domey Benjamin <domeybenjamin1@gmail.com>
2026-01-17 16:09:47 +0000

    feat: enhance PropertyTenantApplication with tenant details and action buttons

--- apps/property-manager/app/modules/properties/property/tenants/applications/application/index.tsx
+++ apps/property-manager/app/modules/properties/property/tenants/applications/application/index.tsx
@@ -1,3 +1,126 @@
+import { Building, Mail, Phone } from 'lucide-react'
+import { Link, Outlet, useLocation, useParams } from 'react-router'
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
+import { Badge } from '~/components/ui/badge'
+import { Button } from '~/components/ui/button'
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '~/components/ui/card'
+import { Separator } from '~/components/ui/separator'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs'
+import { TypographyMuted, TypographyP } from '~/components/ui/typography'
+import { localizedDayjs } from '~/lib/date'
+import { getNameInitials } from '~/lib/misc'
+import { useProperty } from '~/providers/property-provider'
+
+const tenant = {
+	id: 't1',
+	name: 'Gideon Bempong',
+	since: 'Jan 2025',
+	location: 'Osu, Accra',
+	phone: '(233) 277099230',
+	email: 'gideon@example.com',
+	profile: 'https://github.com/shadcn.png',
+	status: 'Active',
+}
+
 export function PropertyTenantApplication() {
-	return <main className="w-full">Single application</main>
+	const { pathname } = useLocation()
+	const { applicationId } = useParams()
+	const { clientUserProperty } = useProperty()
+
+	const baseUrl = `/properties/${clientUserProperty?.property?.id}/tenants/applications/${applicationId}`
+	return (
+		<div className="m-5 grid grid-cols-12 gap-4">
+
+			<div className="col-span-8">
+				<div className='space-y-1'>
+					<h1 className='text-3xl font-bold'>Application Info #123242</h1>
+					<span className='text-sm text-gray-500'>
+						Submitted on{' '}
+						<strong>{localizedDayjs().format('LLLL')}</strong>
+					</span>
+				</div>
+				<Tabs value={pathname}>
+					<TabsContent value={pathname}>
+					</TabsContent>
+				</Tabs>
+			</div>
+			<div className="col-span-4">
+				<div className='flex flex-row items-center justify-end space-x-2 mb-3 w-full'>
+					<Button
+						size="lg"
+						variant={'destructive'}
+					>
+						Cancel
+					</Button>
+					<Button
+						size="lg"
+						className='bg-green-600 hover:bg-green-800'
+					>
+						Approve Application
+					</Button>
+				</div>
+				<Card key={tenant.id} className="shadow-none">
+					<CardHeader className="flex items-start justify-between gap-3">
+						<CardTitle>
+							<Badge
+								variant={tenant.status === 'Active' ? 'secondary' : 'default'}
+								className="px-2 py-1 text-xs"
+							>
+								{tenant.status}
+							</Badge>
+						</CardTitle>
+					</CardHeader>
+
+					<CardContent className="space-y-3">
+						<div className="flex flex-col items-center gap-3">
+							<Avatar className="h-16 w-16">
+								{tenant.profile ? (
+									<AvatarImage src={tenant.profile} alt={tenant.name} />
+								) : (
+									<AvatarFallback>
+										{getNameInitials(tenant.name)}
+									</AvatarFallback>
+								)}
+							</Avatar>
+							<div>
+								<CardTitle className="text-sm font-semibold">
+									{tenant.name}
+								</CardTitle>
+								<TypographyMuted className="text-xs">
+									Rent from {tenant.since}
+								</TypographyMuted>
+							</div>
+						</div>
+
+						<Separator className="my-5" />
+
+						<div>
+							<div className="flex items-center gap-2 text-sm">
+								<Building size={14} className="text-zinc-500" />
+								<TypographyP className="!mt-0">{tenant.location}</TypographyP>
+							</div>
+
+							<div className="flex items-center gap-2 text-sm">
+								<Phone size={14} className="text-zinc-500" />
+								<TypographyP className="!mt-0">{tenant.phone}</TypographyP>
+							</div>
+
+							<div className="flex items-center gap-2 text-sm">
+								<Mail size={14} className="text-zinc-500" />
+								<TypographyP className="!mt-0">{tenant.email}</TypographyP>
+							</div>
+						</div>
+					</CardContent>
+
+					<CardFooter></CardFooter>
+				</Card>
+			</div>
+		</div>
+	)
 }
