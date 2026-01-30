06f05729627a78727ac41da8813e482ee06fb183
Domey Benjamin <domeybenjamin1@gmail.com>
2026-01-30 22:08:50 +0000

    feat: add financial and move-in setup components with form handling

---
+++ apps/property-manager/app/components/external-link.tsx
@@ -0,0 +1,10 @@
+import { Link, type LinkProps } from "react-router";
+
+
+interface ExternalLinkProps extends LinkProps{}
+
+export function ExternalLink(props: ExternalLinkProps) {
+    return (
+        <Link {...props} target="_blank" rel="noopener noreferrer" />
+    )
+}
--- apps/property-manager/app/modules/properties/property/tenants/applications/application/financial/index.tsx
+++ apps/property-manager/app/modules/properties/property/tenants/applications/application/financial/index.tsx
@@ -1,9 +1,42 @@
 import { useParams } from 'react-router'
 import { useProperty } from '~/providers/property-provider'
+import { DatePickerInput } from '~/components/date-picker-input'
+import { Button } from '~/components/ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '~/components/ui/card'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '~/components/ui/form'
+import { Input } from '~/components/ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/select'
 
 export function PropertyTenantApplicationFinancial() {
 	const { applicationId } = useParams()
 	const { clientUserProperty } = useProperty()
 
-	return <div>financial detail</div>
+	return (
+		<Card className="shadow-none">
+			<CardHeader>
+				<CardTitle>Financial Setup</CardTitle>
+				<CardDescription>
+					Setup financial details for the tenant.
+				</CardDescription>
+			</CardHeader>
+
+			<CardContent className="space-y-3">
+				{/* rent collection setup */}
+				{/* security deposit */}
+
+			</CardContent>
+			<CardFooter className="flex justify-end">
+				<div className="flex flex-row items-center space-x-2">
+					<Button disabled>Save</Button>
+				</div>
+			</CardFooter>
+		</Card>
+	);
 }
