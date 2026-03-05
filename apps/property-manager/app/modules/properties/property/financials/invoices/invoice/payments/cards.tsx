b6b4d8bf4f528b1b57e27392d8673da718709176
gidoo5331 <gideonbempong@gmail.com>
2026-03-05 19:03:19 +0000

    edited  payments tab

--- apps/property-manager/app/modules/properties/property/financials/invoices/invoice/payments/cards.tsx
+++ apps/property-manager/app/modules/properties/property/financials/invoices/invoice/payments/cards.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export function TenantPaymentSectionCards({ data }: Props) {
 	return (
-		<div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+		<div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-3">
 			<StatCard
 				title="Tax & Fees"
 				value={formatAmount(data?.taxes || 0)}
