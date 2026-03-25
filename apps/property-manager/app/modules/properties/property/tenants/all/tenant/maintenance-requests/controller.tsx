c5e7d593384f7c275fec79ef63658f895b6bf8b9
Gideon Bempong <gideonbempong533@gmail.com>
2026-03-25 17:02:53 +0000

    add maintenance (#283)
    Co-authored-by: gidoo5331 <gideonbempong@gmail.com>

---
+++ apps/property-manager/app/modules/properties/property/tenants/all/tenant/maintenance-requests/controller.tsx
@@ -0,0 +1,35 @@
+import { ToggleLeft } from 'lucide-react'
+import { FilterSet } from '~/components/filter-set'
+
+const filters: Array<Filter> = [
+	{
+		id: 1,
+		type: 'selector',
+		selectType: 'single',
+		label: 'Status',
+		value: {
+			options: [
+				{ label: 'New', value: 'NEW' },
+				{ label: 'In Progress', value: 'IN_PROGRESS' },
+				{ label: 'In Review', value: 'IN_REVIEW' },
+				{ label: 'Resolved', value: 'RESOLVED' },
+				{ label: 'Canceled', value: 'CANCELED' },
+			],
+			urlParam: 'status',
+			defaultValues: [],
+		},
+		Icon: ToggleLeft,
+	},
+]
+
+export const PropertyTenantMaintenanceRequestsController = () => {
+	return (
+		<div className="flex w-full flex-col gap-2">
+			<div className="w-full rounded-md border p-4">
+				<div className="flex w-full flex-wrap items-center gap-2 text-sm">
+					<FilterSet label="Filters" urlParam="filters" filters={filters} />
+				</div>
+			</div>
+		</div>
+	)
+}
