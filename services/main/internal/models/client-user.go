4eec96bd50613d734ccbe296c549f01599cae9de
Domey Benjamin <domeybenjamin1@gmail.com>
2026-04-02 08:02:23 +0000

    feat: add User model, remove auth fields from ClientUser

--- services/main/internal/models/client-user.go
+++ services/main/internal/models/client-user.go
@@ -3,21 +3,17 @@ package models
 import (
 	"errors"
 
-	"github.com/Bendomey/goutilities/pkg/hashpassword"
-	"github.com/getsentry/raven-go"
 	"gorm.io/gorm"
 )
 
-// ClientUser represents a client user in the system
+// ClientUser represents a membership between a User and a Client
 type ClientUser struct {
 	BaseModelSoftDelete
-	ClientID string `gorm:"not null;index;"`
-	Client   Client
+	UserID string `gorm:"not null;index"`
+	User   User
 
-	Name        string `gorm:"not null;"`
-	PhoneNumber string `gorm:"not null;"`
-	Email       string `gorm:"not null;uniqueIndex"`
-	Password    string `gorm:"not null"`
+	ClientID string `gorm:"not null;index"`
+	Client   Client
 
 	Role string `json:"role" gorm:"not null;"` // OWNER | ADMIN | STAFF
 
@@ -30,24 +26,9 @@ type ClientUser struct {
 	StatusUpdatedBy   *ClientUser
 }
 
-// BeforeCreate hook is called before the data is persisted to db
-func (clientUser *ClientUser) BeforeCreate(tx *gorm.DB) (err error) {
-	// hashes password
-	hashed, err := hashpassword.HashPassword(clientUser.Password)
-	clientUser.Password = hashed
-	if err != nil {
-		raven.CaptureError(err, map[string]string{
-			"function": "ClientUser.BeforeCreate",
-			"action":   "hashing password",
-		})
-		err = errors.New("CannotHashClientUserPassword")
-	}
-	return
-}
-
-// BeforeDelete hook is called before the data is delete so that we dont delete super client user
-func (admin *ClientUser) BeforeDelete(tx *gorm.DB) (err error) {
-	if admin.CreatedByID == nil {
+// BeforeDelete hook prevents deletion of the owner (first) client user
+func (cu *ClientUser) BeforeDelete(tx *gorm.DB) (err error) {
+	if cu.CreatedByID == nil {
 		err = errors.New("CannotDeleteSuperUserForClient")
 	}
 	return
